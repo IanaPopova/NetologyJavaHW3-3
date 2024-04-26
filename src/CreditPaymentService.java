2c0d701d9bd6d94f06230fae160d0efe915db03e
Яна Попова <ynappva@gmail.com>
2024-04-26 23:59:06 +0300

    Code formation

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,12 @@
 public class CreditPaymentService {
-
     public double calculate(int amountOfCredit, double iR, int timeOfPayment) {
 
-        double iRFormula = (iR/(100/12));
-        double iRDegree = Math.pow(1+iRFormula, timeOfPayment);
-      double monthlyPayment = (int) (amountOfCredit * (iRFormula + iRFormula/(iRDegree) - 1));
-//double monthlyRate = (int) (amountOfCredit * (iRDegree)/((iRDegree)-1));
-      // double monthlyPayment = amountOfCredit * (iRFormula/(1-(iRDegree)-1));
+        double iRFormula = (iR / (100 / 12));
+        double iRDegree = Math.pow(1 + iRFormula, timeOfPayment);
+        double monthlyPayment = (int) (amountOfCredit * (iRFormula + iRFormula / (iRDegree) - 1));
+        //double monthlyRate = (int) (amountOfCredit * (iRDegree)/((iRDegree)-1));
+        // double monthlyPayment = amountOfCredit * (iRFormula/(1-(iRDegree)-1));
+
         return (int) monthlyPayment;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+
         int amountOfCredit = 1_000_000;
         double iR = 9.99;
         int timeOfPayment = 12;
-       double monthlyPayment = (int) service.calculate(amountOfCredit, iR, timeOfPayment);
+        double monthlyPayment = (int) service.calculate(amountOfCredit, iR, timeOfPayment);
+
         System.out.println(monthlyPayment);
 
     }
