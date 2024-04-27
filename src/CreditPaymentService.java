8332fc1cb5ad144459dd2427169caad3e6ef2a5a
Яна Попова <ynappva@gmail.com>
2024-04-27 18:29:20 +0300

    formula updated

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,9 @@
 public class CreditPaymentService {
     public double calculate(int amountOfCredit, double iR, int timeOfPayment) {
 
-        double iRFormula = (iR / (100 / 12));
-        double iRDegree = Math.pow(1 + iRFormula, timeOfPayment);
-        double monthlyPayment = (int) (amountOfCredit * (iRFormula + iRFormula / (iRDegree) - 1));
-        //double monthlyRate = (int) (amountOfCredit * (iRDegree)/((iRDegree)-1));
-        // double monthlyPayment = amountOfCredit * (iRFormula/(1-(iRDegree)-1));
+        double iRFormula = (iR / (12*100));
+        double iRDegree = Math.pow(iRFormula, timeOfPayment);
+        double monthlyPayment = (int) amountOfCredit * iRFormula/(1-(1 + iRDegree)-1);
 
         return (int) monthlyPayment;
     }
